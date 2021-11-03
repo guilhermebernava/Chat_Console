cffbe86e8b669edef5d40e2bc3088d7876117d51
Guilherme <guilhermebernava00@gmail.com>
2021-11-03 11:41:58 -0300

    Tudo Funcionando, mas sem Async

---
+++ Server/Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            byte[] data = new byte[1024];
+
+            string input, stringData;
+
+            IPEndPoint IpEnd = new IPEndPoint(IPAddress.Loopback, 9000);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(IpEnd);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+
+                Console.WriteLine(e.ToString());
+
+                return;
+            }
+
+            int recive = server.Receive(data);
+
+            stringData = Encoding.UTF8.GetString(data, 0, recive);
+
+            Console.WriteLine(stringData);
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine("You: " + input);
+                server.Send(Encoding.UTF8.GetBytes(input));
+
+                data = new byte[1024];
+
+                recive = server.Receive(data);
+
+                stringData = Encoding.UTF8.GetString(data, 0, recive);
+
+                byte[] utf8string = System.Text.Encoding.UTF8.GetBytes(stringData);
+
+                Console.WriteLine("Server: " + stringData);
+            }
+            Console.WriteLine("Disconnecting from server...");
+
+            server.Shutdown(SocketShutdown.Both);
+
+            server.Close();
+
+            Console.WriteLine("Disconnected!");
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int recive;
+
+            byte[] data = new byte[1024];
+
+            IPEndPoint IPend = new IPEndPoint(IPAddress.Loopback, 9000);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(IPend);
+            socket.Listen(1);
+
+            Console.WriteLine("Waiting for a client....");
+
+            Socket client = socket.Accept();//cria um novo socket com base numa conexao
+
+            IPEndPoint clientEp = (IPEndPoint)client.RemoteEndPoint;
+
+            Console.WriteLine($"Connected with {clientEp.Address} at port {clientEp.Port}");
+
+            string welcome = " <3 Welcome to server chat <3";
+
+            data = Encoding.UTF8.GetBytes(welcome);//codifica o dado em bytes para mandar pro client
+
+            client.Send(data, data.Length, SocketFlags.None);
+
+            string input;
+
+            while (true)
+            {
+                data = new byte[1024];
+
+                recive = client.Receive(data);
+
+                if(recive == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Client: " + Encoding.UTF8.GetString(data,0,recive));
+                input = Console.ReadLine();
+                Console.WriteLine("You: " + input);
+
+                client.Send(Encoding.UTF8.GetBytes(input));
+
+            }
+            Console.WriteLine("Disconnected from {0}", clientEp.Address);
+
+            client.Close();
+
+            socket.Close();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
